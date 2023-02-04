d23c12129e4a764b16775a24973b671272c49bae
Azhang1234 <zhan4096@purdue.edu>
2023-02-04 17:28:35 -0500

    Initial Commit

---
+++ lesson3/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { BigDog, Component } from './component';
+function App() {
+  return (
+    <div>
+        <BigDog/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson3/src/component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export function Component() {
+    let name = "Alan"
+    return (
+        <p>{name}</p>
+    );
+}
+
+export function BigDog(){
+    let sammy = "Samuel Costa"
+    return (
+        <p>{sammy}</p>
+    )
+}
